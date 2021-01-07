74df7dbcaeb352d255547d5c2737b4d14878d7d7
Aseiide <eiseiide@gmail.com>
2021-01-07 22:21:39 +0900

    /signupへのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "cafes#index"
   resources :cafes
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
