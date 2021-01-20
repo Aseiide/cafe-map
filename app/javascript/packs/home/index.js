91bd330f78cbda705251b74f6d26a33eb5f608b4
Aseiide <eiseiide@gmail.com>
2021-01-20 14:15:48 +0900

    初期値を代入

--- app/javascript/packs/home/index.js
+++ app/javascript/packs/home/index.js
@@ -1,19 +1,19 @@
-const map
+const map = null;
 
 function initMap() {
-  geocoder = new google.maps.Geocoder()
+  geocoder = new google.maps.Geocoder();
 
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 40.7828, lng: -73.9653 },
     zoom: 12,
   });
 
   marker = new google.maps.Marker({
     position: { lat: 40.7828, lng: -73.9653 },
-    map: map
+    map: map,
   });
 }
 
 window.onload = function () {
   initMap();
-}
+};
