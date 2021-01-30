17b5188a5a9a1994869a5c522f91fc3eade46fcc
Aseiide <eiseiide@gmail.com>
2021-01-30 12:19:02 +0900

    cavesモデルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_113045) do
+ActiveRecord::Schema.define(version: 2021_01_30_031520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "caves", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
-    t.text "description"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "memo"
   end
 
 end
