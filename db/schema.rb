1e46caf0a6ad0a2d57b57ea928dca1d95885f5e7
Aseiide <eiseiide@gmail.com>
2021-02-07 00:38:55 +0900

    DBの整合性を取るため

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_025447) do
+ActiveRecord::Schema.define(version: 2021_02_04_100337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+  create_table "cafes", force: :cascade do |t|
+    t.string "address"
+    t.float "longitude"
+    t.float "latitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
