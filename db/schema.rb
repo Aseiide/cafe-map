7bdb18b1afe331a9bae290f551856314d24a4d6a
Aseiide <eiseiide@gmail.com>
2021-02-07 00:35:59 +0900

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_100337) do
+ActiveRecord::Schema.define(version: 2021_01_30_025447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cafes", force: :cascade do |t|
-    t.string "address"
-    t.float "longitude"
-    t.float "latitude"
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
