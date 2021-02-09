383d96e6f37ea12f6078bbd48922eb86b65743b5
Aseiide <eiseiide@gmail.com>
2021-02-09 15:25:54 +0900

    viewに緯度経度を表示

--- app/controllers/admin/cafes_controller.rb
+++ app/controllers/admin/cafes_controller.rb
@@ -1,10 +1,18 @@
 class Admin::CafesController < Admin::BaseController
+  def index
+  end
+
   def new
-    # binding.pry
-    @cafe = Cafe.new
+    location = params[:location]
+    # location->入力される住所,文字列
+    params = URI.encode_www_form({address: location})
+    uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=#{params}&key=AIzaSyDU2acEBi2vsAS3MNo8fZVFzB_Ei1qXezg")
+    response = Net::HTTP.get_response(uri)
+    result = JSON.parse(response.body)
+    @latitude = result["results"][0]["geometry"]["location"]["lat"]
+    @longitude = result["results"][0]["geometry"]["location"]["lng"]
   end
 
   def create
-    binding.pry
   end
 end
