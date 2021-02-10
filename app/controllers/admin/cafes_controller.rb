a42524b519bbbff92875840a8dc10c8920dab6fc
Aseiide <eiseiide@gmail.com>
2021-02-10 21:58:47 +0900

    レビューを受けて修正

--- app/controllers/admin/cafes_controller.rb
+++ app/controllers/admin/cafes_controller.rb
@@ -1,18 +1,13 @@
 class Admin::CafesController < Admin::BaseController
-  def index
-  end
-
   def new
-    location = params[:location]
-    # location->入力される住所,文字列
+    def location
+      params[:location]
+    end
     params = URI.encode_www_form({address: location})
-    uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=#{params}&key=AIzaSyDU2acEBi2vsAS3MNo8fZVFzB_Ei1qXezg")
+    uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=#{params}&key=#{ENV["MAP_API_KEY"]}")
     response = Net::HTTP.get_response(uri)
     result = JSON.parse(response.body)
     @latitude = result["results"][0]["geometry"]["location"]["lat"]
     @longitude = result["results"][0]["geometry"]["location"]["lng"]
-  end
-
-  def create
   end
 end
