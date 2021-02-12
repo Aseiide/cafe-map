2ff6867381f1e4305b62c6b5f4c11c87dcc48144
Aseiide <eiseiide@gmail.com>
2021-02-12 18:17:13 +0900

    管理画面から住所を入力してrootにピンを立てるため

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,5 +1,14 @@
 class CafesController < ApplicationController
-  def index 
+  def index
+    def location
+      params[:location]
+    end
+    params = URI.encode_www_form({address: location})
+    uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=#{params}&key=#{ENV["MAP_API_KEY"]}")
+    response = Net::HTTP.get_response(uri)
+    result = JSON.parse(response.body)
+    @latitude = result["results"][0]["geometry"]["location"]["lat"]
+    @longitude = result["results"][0]["geometry"]["location"]["lng"]
   end
 
   def show
--- app/javascript/packs/home/index.js
+++ app/javascript/packs/home/index.js
@@ -7,7 +7,16 @@ function initMap(){
     // 取得した位置情報を中心に表示
     map = new google.maps.Map(document.getElementById('map'), {
       center: LatLng,
-      zoom: 15
+      zoom: 14
+    });
+
+    //緯度経度を定義
+    const latitude = parseFloat(document.getElementById('latitude').textContent);
+    const longitude = parseFloat(document.getElementById('longitude').textContent);
+    //取得した緯度経度にピンを立てる
+    const marker = new google.maps.Marker({
+      position:{lat: latitude, lng: longitude},
+      map: map,
     });
   });
 }
