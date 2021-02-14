dc549b2a5966e01beeb767cb2f83eefe95dfa0fe
Aseiide <eiseiide@gmail.com>
2021-02-14 18:14:28 +0900

    不要のため

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,11 +1,8 @@
 class CafesController < ApplicationController
   def create
     @cafe = Cafe.new(cafe_params)
-    # @latitude = Cafe.new
-    # @longitude = Cafe.new
     #保存
     @cafe.save
-    binding.pry
   end
   def cafe_params
     params.require(:cafe).permit(:address)
