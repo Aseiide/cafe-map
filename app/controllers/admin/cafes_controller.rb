9fd7a6ba2af807fc6ed235c9e197a908d3e35e79
Aseiide <eiseiide@gmail.com>
2021-02-15 19:40:59 +0900

    wip

--- app/controllers/admin/cafes_controller.rb
+++ app/controllers/admin/cafes_controller.rb
@@ -4,8 +4,6 @@ class Admin::CafesController < Admin::BaseController
     params = URI.encode_www_form({address: address})
     uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=#{params}&key=#{ENV["MAP_API_KEY"]}")
     response = Net::HTTP.get_response(uri)
-    result = JSON.parse(response.body)
-    @latitude = result["results"][0]["geometry"]["location"]["lat"]
-    @longitude = result["results"][0]["geometry"]["location"]["lng"]
+    @result = JSON.parse(response.body)
   end
 end
--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,8 +1,13 @@
 class CafesController < ApplicationController
   def create
-    @cafe = Cafe.new(cafe_params)
+    @cafe = Cafe.new(
+      cafe_params,
+      latitude: @result["results"][0]["geometry"]["location"]["lat"],
+      longitude: @result["results"][0]["geometry"]["location"]["lng"]
+    )
     #保存
     @cafe.save
+    redirect_to cafes_path
   end
   def cafe_params
     params.require(:cafe).permit(:address)
