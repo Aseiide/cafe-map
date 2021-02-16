b5a8ec5836e11e5d42d7d5a1352e4427e6577f3c
Aseiide <eiseiide@gmail.com>
2021-02-16 14:18:08 +0900

    住所、緯度、経度を取得

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,17 +1,14 @@
 class CafesController < ApplicationController
   def create
-    address = params[:cafe]["address"]
+    address = params[:cafe]
     params = URI.encode_www_form({address: address})
-    uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=#{params}&key=#{ENV["MAP_API_KEY"]}")
+    uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?#{params}&key=#{ENV["MAP_API_KEY"]}")
     response = Net::HTTP.get_response(uri)
-    result = JSON.parse(response.body)
-    @latitude = result["results"][0]["geometry"]["location"]["lat"],
-    @longitude = result["results"][0]["geometry"]["location"]["lng"]
-    binding.pry
+    @result = JSON.parse(response.body)
     @cafe = Cafe.new(
-      cafe_params,
-      latitude: @latitude,
-      longitude: @longitude
+      address: cafe_params,
+      latitude: @result["results"][0]["geometry"]["location"]["lat"],
+      longitude: @result["results"][0]["geometry"]["location"]["lng"]
     )
     binding.pry
     #保存
@@ -19,6 +16,6 @@ class CafesController < ApplicationController
     redirect_to cafes_path
   end
   def cafe_params
-    params.require(:cafe).permit(:address)
+    params.require(:cafe).permit(:address, :latitude, :longitude)
   end
 end
