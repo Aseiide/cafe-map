1a29eb947c6e1df70c946afb070a6278000f7b73
Aseiide <eiseiide@gmail.com>
2021-02-16 11:13:13 +0900

    フォームから受け取った値でAPIを叩くため

--- app/controllers/admin/cafes_controller.rb
+++ app/controllers/admin/cafes_controller.rb
@@ -1,14 +1,5 @@
 class Admin::CafesController < Admin::BaseController
   def new
-    address = params[:address]
-    params = URI.encode_www_form({address: address})
-    uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=#{params}&key=#{ENV["MAP_API_KEY"]}")
-    response = Net::HTTP.get_response(uri)
-    result = JSON.parse(response.body)
-    @latlng = Cafe.new(
-    latitude: result["results"][0]["geometry"]["location"]["lat"],
-    longitude: result["results"][0]["geometry"]["location"]["lng"]
-    )
-    binding.pry
+    @cafe = Cafe.new
   end
 end
--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,9 +1,17 @@
 class CafesController < ApplicationController
   def create
+    address = params[:cafe]["address"]
+    params = URI.encode_www_form({address: address})
+    uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=#{params}&key=#{ENV["MAP_API_KEY"]}")
+    response = Net::HTTP.get_response(uri)
+    result = JSON.parse(response.body)
+    @latitude = result["results"][0]["geometry"]["location"]["lat"],
+    @longitude = result["results"][0]["geometry"]["location"]["lng"]
+    binding.pry
     @cafe = Cafe.new(
-      cafe_params
-      # latitude: @latitude,
-      # longitude: @longitude
+      cafe_params,
+      latitude: @latitude,
+      longitude: @longitude
     )
     binding.pry
     #保存
