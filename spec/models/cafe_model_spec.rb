e21af0342a3ea8eeb1b9ec61f2d6ef702401c051
Aseiide <eiseiide@gmail.com>
2021-02-21 22:45:44 +0900

    テストを追加

--- spec/models/cafe_model_spec.rb
+++ spec/models/cafe_model_spec.rb
@@ -2,7 +2,10 @@ require "rails_helper"
 
 RSpec.describe Cafe, type: :model do
   it "is invalid without an address" do
-    hoge = Cafe.new(address: "")
+    hoge = Cafe.new(
+      name: "",
+      address: ""
+    )
     hoge.valid?
     expect(hoge.errors[:address]).to include("can't be blank")
   end
