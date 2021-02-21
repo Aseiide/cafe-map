5b66c1a4a4968273a184391885ed2278203482a4
Aseiide <eiseiide@gmail.com>
2021-02-21 22:53:37 +0900

    不要なコメントを削除

--- app/controllers/admin/cafes_controller.rb
+++ app/controllers/admin/cafes_controller.rb
@@ -7,15 +7,12 @@ class Admin::CafesController < Admin::BaseController
     uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?#{params}&key=#{ENV["MAP_API_KEY"]}")
     response = Net::HTTP.get_response(uri)
     @result = JSON.parse(response.body)
-    # binding.pry
     @cafe = Cafe.new(
       name: cafe_params["name"],
       address: cafe_params["address"],
       latitude: @result["results"][0]["geometry"]["location"]["lat"],
       longitude: @result["results"][0]["geometry"]["location"]["lng"]
     )
-    # binding.pry
-    #保存
     if @cafe.save
       puts "保存に成功"
       redirect_to cafes_path
