ca16784a57ca5341f537f9104388c16580b60488
Aseiide <eiseiide@gmail.com>
2021-02-22 17:19:58 +0900

    テストを追加

--- spec/models/cafe_model_spec.rb
+++ spec/models/cafe_model_spec.rb
@@ -1,12 +1,18 @@
 require "rails_helper"
 
 RSpec.describe Cafe, type: :model do
-  it "is invalid without an address" do
+  it "nameとaddressが空では保存できない" do
     hoge = Cafe.new(
       name: "",
       address: ""
     )
-    hoge.valid?
-    expect(hoge.errors[:address]).to include("can't be blank")
+    expect(hoge.save).to be_falsey #nameとaddressが空なのでfalseを期待する
+  end
+  it "nameとaddressを埋めると保存できる" do
+    foo = Cafe.new(
+      name: "Cafe1",
+      address: "東京都千代田区1-1"
+    )
+    expect(foo.save).to be_truthy
   end
 end
