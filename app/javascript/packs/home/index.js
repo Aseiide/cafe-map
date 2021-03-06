f5967f84e24fef126fd1a271551568174e448b3d
Aseiide <eiseiide@gmail.com>
2021-03-06 18:59:21 +0900

    DOMから要素を取得してピンを表示するように記述

--- app/javascript/packs/home/index.js
+++ app/javascript/packs/home/index.js
@@ -2,12 +2,21 @@ function initMap(){
   geocoder = new google.maps.Geocoder();
   // 位置情報を取得
   navigator.geolocation.getCurrentPosition(function (position) {
-    LatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    // LatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
+    //緯度経度を定義
+    const lat = document.getElementById("lat").dataset.lat
+    const lng = document.getElementById("lng").dataset.lng
+    const Latlng = {lat: parseFloat(lat), lng: parseFloat(lng)} 
     // 取得した位置情報を中心に表示
     map = new google.maps.Map(document.getElementById('map'), {
-      center: LatLng,
+      center: Latlng,
       zoom: 15
+    });
+    new google.maps.Marker({
+      position: Latlng,
+      map,
+      title: "Hello World!",
     });
   });
 }
