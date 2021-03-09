2912bbfdd805f292b74b80d814e8adaff4070829
Aseiide <eiseiide@gmail.com>
2021-03-09 17:30:01 +0900

    現在地を起点に表示するため

--- app/javascript/packs/home/index.js
+++ app/javascript/packs/home/index.js
@@ -2,19 +2,19 @@ function initMap(){
   geocoder = new google.maps.Geocoder();
   // 位置情報を取得
   navigator.geolocation.getCurrentPosition(function (position) {
-    // LatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    LatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: LatLng,
+      zoom: 15
+    });
 
     //緯度経度を定義
     const lat = document.getElementById("lat").dataset.lat
     const lng = document.getElementById("lng").dataset.lng
-    const Latlng = {lat: parseFloat(lat), lng: parseFloat(lng)} 
-    // 取得した位置情報を中心に表示
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: Latlng,
-      zoom: 15
-    });
+    const Store = {lat: parseFloat(lat), lng: parseFloat(lng)} 
+
     new google.maps.Marker({
-      position: Latlng,
+      position: Store,
       map,
       title: "Hello World!",
     });
