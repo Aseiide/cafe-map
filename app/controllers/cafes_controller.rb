209e60e0f70c295c8b474f92e49d19b728f5c34b
Aseiide <eiseiide@gmail.com>
2021-03-09 21:49:16 +0900

    wip

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,6 +1,6 @@
 class CafesController < ApplicationController
   def index
-    @cafe_latitude = Cafe.last.latitude
-    @cafe_longitude = Cafe.last.longitude
+    @cafe_latitude = Cafe.select("latitude").last(5)
+    @cafe_longitude = Cafe.select("longitude").last(5)
   end
 end
--- app/javascript/packs/home/index.js
+++ app/javascript/packs/home/index.js
@@ -1,3 +1,5 @@
+let marker = [];
+let markerData = [];
 function initMap(){
   geocoder = new google.maps.Geocoder();
   // 位置情報を取得
@@ -8,16 +10,37 @@ function initMap(){
       zoom: 15
     });
 
-    //緯度経度を定義
     const lat = document.getElementById("lat").dataset.lat
     const lng = document.getElementById("lng").dataset.lng
     const Store = {lat: parseFloat(lat), lng: parseFloat(lng)} 
 
-    new google.maps.Marker({
-      position: Store,
-      map,
-      title: "Hello World!",
-    });
+    for (var i = 0; i<markerData.length; i++){
+      let id = markerData[i]["id"]
+
+      markerLatLng = new google.maps.LatLng({
+        lat: lat[i],
+        lng: lng[i]
+      });
+
+      console.log(lat[i]);
+      console.log(lng[i]);
+
+      marker[i] = new google.maps.Marker({
+        position: markerLatLng,
+        map:map
+      });
+    }
+
+    //緯度経度を定義
+    // const lat = document.getElementById("lat").dataset.lat
+    // const lng = document.getElementById("lng").dataset.lng
+    // const Store = {lat: parseFloat(lat), lng: parseFloat(lng)} 
+
+    // new google.maps.Marker({
+    //   position: Store,
+    //   map,
+    //   title: "Hello World!",
+    // });
   });
 }
 window.onload = function() {
