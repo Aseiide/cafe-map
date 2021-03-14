81dad62adb12f43e56c55029a1d751ed49f3290e
Aseiide <eiseiide@gmail.com>
2021-03-14 17:56:59 +0900

    不要なコメントを削除

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,20 +1,4 @@
 class CafesController < ApplicationController
   def index
     @all_cafe_position = Cafe.all.pluck(:latitude, :longitude).to_json
-    # .map { |latitude, longitude| { latitude: latitude, longitude: longitude } }
-    # render json: @all_cafe_position
-    # @cafe_latitude = cafe_latitude
-    # @cafe_longitude = cafe_longitude
   end
-
-  private
-
-    # def cafe_latitude
-    #   @all_cafe.pluck(:latitude)
-    # end
-
-    # def cafe_longitude
-    #   @all_cafe.pluck(:longitude)
-    # end
-end
-
--- app/javascript/packs/home/index.js
+++ app/javascript/packs/home/index.js
@@ -1,12 +1,9 @@
 let marker = [];
 let markerData = JSON.parse(document.querySelector("#marker-data").dataset.position);
-// let markerData = document.getElementById("lng").dataset.lng;
-// let latArray = document.getElementById("lat").dataset.lat
-// let lngArray = document.getElementById("lng").dataset.lng
 
 function initMap() {
   geocoder = new google.maps.Geocoder();
-  // 位置情報を取得
+  // 位置情報を取得して中心に表示
   navigator.geolocation.getCurrentPosition(function(position) {
     LatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
     map = new google.maps.Map(document.getElementById('map'), {
@@ -15,11 +12,6 @@ function initMap() {
     });
 
     for (var i = 0; i < markerData.length; i++) {
-      // let id = markerData[i]["id"]
-      // const lat = document.getElementById("lat").dataset.lat
-      // const lng = document.getElementById("lng").dataset.lng
-      //const Store = { lat: parseFloat(lat), lng: parseFloat(lng) }
-
       markerLatLng = new google.maps.LatLng({
         lat: markerData[i][0],
         lng: markerData[i][1]
@@ -30,16 +22,6 @@ function initMap() {
         map: map
       });
     }
-
-    // // 緯度経度を渡して、ピン表示
-    // const lat = 35.658584
-    // const lng = 139.7454316
-    // const Store = { lat: parseFloat(lat), lng: parseFloat(lng) }
-
-    // new google.maps.Marker({
-    //     position: Store,
-    //     map
-    // });
   });
 }
 
