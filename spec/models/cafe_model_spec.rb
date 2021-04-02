1dc945e6331cd9b472f7c976baaee2f341ff2bc5
Aseiide <eiseiide@gmail.com>
2021-04-02 17:37:06 +0900

    add test

--- spec/models/cafe_model_spec.rb
+++ spec/models/cafe_model_spec.rb
@@ -16,4 +16,28 @@ RSpec.describe Cafe, type: :model do
     )
     expect(hoge.save).to be_falsey
   end
+
+  it "正しいnameで正確な住所では保存できる" do
+    qux = Cafe.new(
+      name: "Test Roaster",
+      address: "福岡県北九州市八幡西区西鳴水1-1-1"
+    )
+    expect(qux.save).to be_truthy
+  end
+
+  it "正しいnameで都道府県なしの住所では保存できない" do
+    quxqux = Cafe.new(
+      name: "Test Roaster",
+      address: "北九州市八幡西区西鳴水1-1-1"
+    )
+    expect(quxqux.save).to be_falsey
+  end
+
+  it "正しいnameで海外の住所では保存できない" do
+    quux = Cafe.new(
+      name: "Test Roaster",
+      address: "ホワイトハウス 1600 Pennsylvania Avenue NW, Washington, DC 20500 アメリカ合衆国"
+    )
+    expect(quux.save).to be_falsey
+  end
 end
