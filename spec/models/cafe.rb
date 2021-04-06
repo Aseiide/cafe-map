31b01f8a7052b1ad5d204b6560cc9b396b992d00
Aseiide <eiseiide@gmail.com>
2021-04-06 23:19:15 +0900

    異常系のテストを追加

--- spec/models/cafe.rb
+++ spec/models/cafe.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Cafe, type: :model do
-  context "住所" do
+  context "住所について" do
     it "入力されている時、有効である" do
       hoge = Cafe.new(
         name: "Test Roaster",
@@ -9,7 +9,14 @@ RSpec.describe Cafe, type: :model do
       )
       expect(hoge).to be_valid
      end
-    # it "ない時、無効である"
+    
+    it "ない時、無効である" do
+      hogehoge = Cafe.new(
+        name: "Test Roaster",
+        address: ""
+      )
+      expect(hogehoge).not_to be_valid
+    end
 
     it "長さが50文字以下の時、有効である" do
       fuga = Cafe.new(
@@ -18,7 +25,15 @@ RSpec.describe Cafe, type: :model do
       )
       expect(fuga).to be_valid
     end
-    # it "長さが51文字以上の時、無効である"
+
+    it "長さが51文字以上の時、無効である" do
+      fugafuga = Cafe.new(
+        name: "Test Roaster",
+        address: "東京都千代田区千代田" + "1-"*45
+      )
+      expect(fugafuga).not_to be_valid
+    end
+
     it "都道府県ありの時、有効である" do
       piyo = Cafe.new(
         name: "Test Roaster",
@@ -26,6 +41,13 @@ RSpec.describe Cafe, type: :model do
       )
       expect(piyo).to be_valid
     end
-    # it "都道府県なしの時、無効である"
+
+    it "都道府県なしの時、無効である" do
+      piyopiyo = Cafe.new(
+        name: "Test Roaster",
+        address: "福岡市博多区博多駅中央街１−１"
+      )
+      expect(piyopiyo).not_to be_valid
+    end
   end
 end
