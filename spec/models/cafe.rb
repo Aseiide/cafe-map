16b3ba756f74ee72e2eeb31ed4fe45117e69f6c4
Aseiide <eiseiide@gmail.com>
2021-04-06 13:20:22 +0900

    テストコードを日本語で書いた

--- spec/models/cafe.rb
+++ spec/models/cafe.rb
@@ -1,51 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Cafe, type: :model do
-  it "addressが空でないとき保存できる" do
-    foo = Cafe.new(
-      name: "Test",
-      address: "東京都千代田区1-1"
-    )
-    expect(foo.save).to be_truthy
-  end
-
-  it "addressが空のとき保存できない" do
-    hoge = Cafe.new(
-      name: "Test",
-      address: ""
-    )
-    expect(hoge.save).to be_falsey
-  end
-
-  it "addressが50文字以下の時、保存できる" do
-    qux = Cafe.new(
-      name: "Test",
-      address: "福岡県北九州市小倉北区浅野１丁目１−１"
-    )
-    expect(qux.save).to be_truthy
-  end
-
-  it "addressが51文字以上の時、保存できない" do
-    quxqux = Cafe.new(
-      name: "Test",
-      address: "a" * 51
-    )
-    expect(quxqux.save).to be_falsey
-  end
-
-  it "都道府県が書かれているとき、保存できる" do
-    quux = Cafe.new(
-      name: "Test",
-      address: "福岡県福岡市博多区博多駅中央街１−１"
-    )
-    expect(quux.save).to be_truthy
-  end
-
-  it "都道府県が書かれていない時、保存できない" do
-    quuxquuux = Cafe.new(
-      name: "Test",
-      address: "北九州市小倉北区浅野１丁目１−１"
-    )
-    expect(quuxquuux.save).to be_falsey
-  end
+ it "住所入力されている時、有効である"
+ it "住所がない時、無効である"
+ it "住所の長さが50文字以下の時、有効である"
+ it "住所の長さが51文字以上の時、無効である"
+ it "都道府県ありの時、有効である"
+ it "都道府県なしの時、無効である"
 end
