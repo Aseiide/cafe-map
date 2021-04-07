1b5a58181b7164b5fe3164c567b8ea68be21b3ce
Aseiide <eiseiide@gmail.com>
2021-04-07 17:58:56 +0900

    rename

--- spec/models/cafe.rb
+++ spec/models/cafe.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Cafe, type: :model do
-  let(:name) {Cafe.new(name: "Test Roaster")}
+  let(:cafe_name) {Cafe.new(name: "Test Roaster")}
   context "住所について" do
     it "入力されている時、有効である" do
       cafe = Cafe.new(
-        name: name,
+        name: cafe_name,
         address: "東京都千代田区千代田1-1"
       )
       expect(cafe).to be_valid
@@ -13,7 +13,7 @@ RSpec.describe Cafe, type: :model do
     
     it "ない時、無効である" do
       cafe = Cafe.new(
-        name: name,
+        name: cafe_name,
         address: ""
       )
       expect(cafe).not_to be_valid
@@ -21,7 +21,7 @@ RSpec.describe Cafe, type: :model do
 
     it "長さが50文字以下の時、有効である" do
       cafe = Cafe.new(
-        name: name,
+        name: cafe_name,
         address: "東京都千代田区丸の内１丁目"
       )
       expect(cafe).to be_valid
@@ -29,7 +29,7 @@ RSpec.describe Cafe, type: :model do
 
     it "長さが51文字以上の時、無効である" do
       cafe = Cafe.new(
-        name: name,
+        name: cafe_name,
         address: "東京都千代田区千代田" + "1-"*45
       )
       expect(cafe).not_to be_valid
@@ -37,7 +37,7 @@ RSpec.describe Cafe, type: :model do
 
     it "都道府県ありの時、有効である" do
       cafe = Cafe.new(
-        name: name,
+        name: cafe_name,
         address: "福岡県福岡市博多区博多駅中央街１−１"
       )
       expect(cafe).to be_valid
@@ -45,7 +45,7 @@ RSpec.describe Cafe, type: :model do
 
     it "都道府県なしの時、無効である" do
       cafe = Cafe.new(
-        name: name,
+        name: cafe_name,
         address: "福岡市博多区博多駅中央街１−１"
       )
       expect(cafe).not_to be_valid
