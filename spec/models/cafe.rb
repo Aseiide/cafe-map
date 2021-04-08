b466e59c9a0bfe301e19649c3451199a8f97fafa
Aseiide <eiseiide@gmail.com>
2021-04-08 15:33:44 +0900

    letを使って変数を再定義

--- spec/models/cafe.rb
+++ spec/models/cafe.rb
@@ -1,54 +1,45 @@
 require "rails_helper"
 
 RSpec.describe Cafe, type: :model do
-  let(:cafe_name) {"Test Roaster"}
-  context "住所について" do
-    it "入力されている時、有効である" do
-      cafe = Cafe.new(
-        name: cafe_name,
-        address: "東京都千代田区千代田1-1"
-      )
-      expect(cafe).to be_valid
-     end
-    
-    it "ない時、無効である" do
-      cafe = Cafe.new(
-        name: cafe_name,
-        address: ""
-      )
-      expect(cafe).not_to be_valid
+
+  describe "住所について" do
+    let(:cafe) { Cafe.new( name: cafe_name, address: address ) }
+    let(:cafe_name) { "Test Roaster" }
+    let(:address) { "東京都千代田区丸の内１丁目" }
+
+    context "入力されている時、有効である" do
+
+      it { expect(cafe).to be_valid }
     end
 
-    it "長さが50文字以下の時、有効である" do
-      cafe = Cafe.new(
-        name: cafe_name,
-        address: "東京都千代田区丸の内１丁目"
-      )
-      expect(cafe).to be_valid
+    context "ない時、無効である" do
+      let(:address) { "" }
+
+      it { expect(cafe).not_to be_valid }
     end
 
-    it "長さが51文字以上の時、無効である" do
-      cafe = Cafe.new(
-        name: cafe_name,
-        address: "東京都千代田区千代田" + "1-"*45
-      )
-      expect(cafe).not_to be_valid
+    context "長さが50文字以下の時、有効である" do
+      let(:address) { "東京都千代田区丸の内１丁目" }
+
+      it { expect(cafe).to be_valid }
     end
 
-    it "都道府県ありの時、有効である" do
-      cafe = Cafe.new(
-        name: cafe_name,
-        address: "福岡県福岡市博多区博多駅中央街１−１"
-      )
-      expect(cafe).to be_valid
+    context "長さが51文字以上の時、無効である" do
+      let(:address) { "東京都千代田区千代田" + "1-"*45 }
+
+      it { expect(cafe).not_to be_valid }
     end
 
-    it "都道府県なしの時、無効である" do
-      cafe = Cafe.new(
-        name: cafe_name,
-        address: "福岡市博多区博多駅中央街１−１"
-      )
-      expect(cafe).not_to be_valid
+    context "都道府県ありの時、有効である" do
+      let(:address) { "福岡県福岡市博多区博多駅中央街１−１" }
+
+      it { expect(cafe).to be_valid }
+    end
+
+    context "都道府県なしの時、無効である" do
+      let(:address) { "福岡市博多区博多駅中央街１−１" }
+
+      it { expect(cafe).not_to be_valid }
     end
   end
 end
