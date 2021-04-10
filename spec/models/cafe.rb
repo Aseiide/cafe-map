ccfc067c5870150b6dfcd8cbcfe6a193bd44965b
Aseiide <eiseiide@gmail.com>
2021-04-10 18:49:43 +0900

    名前のテストを追加

--- spec/models/cafe.rb
+++ spec/models/cafe.rb
@@ -42,4 +42,32 @@ RSpec.describe Cafe, type: :model do
       it { expect(cafe).not_to be_valid }
     end
   end
+
+  describe "名前について" do
+    let(:cafe) { Cafe.new( name: cafe_name, address: address ) }
+    let(:cafe_name) { "Test Roaster" }
+    let(:address) { "東京都千代田区丸の内１丁目" }
+
+    context "入力されているとき、有効である" do
+      it { expect(cafe).to be_valid }
+    end
+
+    context "空文字のとき、無効である" do
+      let(:cafe_name) { "" }
+
+      it { expect(cafe).not_to be_valid }
+    end
+
+    context "40文字以下の時、有効である" do
+      let(:cafe_name) { "a" * 40 }
+
+      it { expect(cafe).to be_valid }
+    end
+
+    context "41文字以上の時、無効である" do
+      let(:cafe_name) { "a" * 41 }
+
+      it { expect(cafe).not_to be_valid }
+    end
+  end
 end
