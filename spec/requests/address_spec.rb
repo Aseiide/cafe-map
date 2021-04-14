0a5fe313d159a26510d3a2b8b1a8f1a373478bc3
Aseiide <eiseiide@gmail.com>
2021-04-14 18:22:08 +0900

    context do として正しいspecにした

--- spec/requests/address_spec.rb
+++ spec/requests/address_spec.rb
@@ -3,28 +3,32 @@ require "rails_helper"
 RSpec.describe "API Test", type: :request do
   describe "アプリケーションのエンドポイントにアクセスできるか" do
 
-    context "GETで /cafes にアクセスした時"
-    it "HTTPステータスコードが200でOKとなる" do
-      get cafes_path
-      expect(response).to have_http_status(200)
+    context "GETで /cafes にアクセスした時" do
+      it "HTTPステータスコードが200でOKとなる" do
+        get cafes_path
+        expect(response).to have_http_status(200)
+      end
+    end
+  
+    context "GETで / にアクセスした時" do
+      it "HTTPステータスコードが200でOKとなる" do
+        get root_path
+        expect(response).to have_http_status(200)
+      end
     end
 
-    context "GETで / にアクセスした時"
-    it "HTTPステータスコードが200でOKとなる" do
-      get root_path
-      expect(response).to have_http_status(200)
+    context "GETで /admin/cafes/new にアクセスした時" do
+      it "HTTPステータスコードが200でOKとなる" do
+        get new_admin_cafe_path
+        expect(response).to have_http_status(200)
+      end
     end
 
-    context "GETで /admin/cafes/new にアクセスした時"
-    it "HTTPステータスコードが200でOKとなる" do
-      get new_admin_cafe_path
-      expect(response).to have_http_status(200)
-    end
-
-    context "すべてのパラメータが揃っている時"
-    it "/cafes にリダイレクトする" do
-      post admin_cafes_path, params:{ cafe: {address:"東京都墨田区押上１-１−２", name: "Test Roaster", latitude: "35.7100069", longitude: "139.8108103"} }
-      expect(response).to redirect_to cafes_path
+    context "すべてのパラメータが揃っている時" do
+      it "/cafes にリダイレクトする" do
+        post admin_cafes_path, params:{ cafe: {address:"東京都墨田区押上１-１−２", name: "Test Roaster", latitude: "35.7100069", longitude: "139.8108103"} }
+        expect(response).to redirect_to cafes_path
+      end
     end
   end
 end
