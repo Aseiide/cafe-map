beed7b85507f871743b3990740ec04046a5a6cd0
Aseiide <eiseiide@gmail.com>
2021-04-14 11:15:55 +0900

    letを用いて変数をまとめる

--- spec/models/cafe.rb
+++ spec/models/cafe.rb
@@ -3,55 +3,59 @@ require "rails_helper"
 RSpec.describe Cafe, type: :model do
 
   describe "住所について" do
+    let(:cafe){ build(:cafe, name: name, address: address) }
+    let(:name){ "Test Roaster" }
+    let(:address){ "東京都墨田区押上１-１−２" }
+
     context "入力されている時、有効である" do
-      cafe = FactoryBot.build(:cafe)
       it { expect(cafe).to be_valid }
     end
 
     context "ない時、無効である" do
-      cafe = FactoryBot.build(:cafe, address: "")
+      let (:address){""}
       it { expect(cafe).not_to be_valid }
     end
 
     context "長さが50文字以下の時、有効である" do
-      cafe = FactoryBot.build(:cafe)
       it { expect(cafe).to be_valid }
     end
 
     context "長さが51文字以上の時、無効である" do
-      cafe = FactoryBot.build(:cafe, address: "東京都千代田区千代田" + "1-"*45)
+      let (:address) {"東京都墨田区押" + "1-"*22}
       it { expect(cafe).not_to be_valid }
     end
 
     context "都道府県ありの時、有効である" do
-      cafe = FactoryBot.build(:cafe)
       it { expect(cafe).to be_valid }
     end
 
     context "都道府県なしの時、無効である" do
-      cafe = FactoryBot.build(:cafe, address: "墨田区押上１-１−２")
+      let(:address){ "墨田区押上１-１−２" }
       it { expect(cafe).not_to be_valid }
     end
   end
 
   describe "名前について" do
+    let(:cafe){ build(:cafe, name: name, address: address) }
+    let(:name){ "Test Roaster" }
+    let(:address){ "東京都墨田区押上１-１−２" }
+
     context "入力されているとき、有効である" do
-      cafe = FactoryBot.build(:cafe)
       it { expect(cafe).to be_valid }
     end
 
     context "空文字のとき、無効である" do
-      cafe = FactoryBot.build(:cafe, name: "")
+      let(:name){""}
       it { expect(cafe).not_to be_valid }
     end
 
     context "40文字以下の時、有効である" do
-      cafe = FactoryBot.build(:cafe, name: "a" * 40)
+      let(:name){ "a" * 40 }
       it { expect(cafe).to be_valid }
     end
 
     context "41文字以上の時、無効である" do
-      cafe = FactoryBot.build(:cafe, name: "a" * 41)
+      let(:name){ "a"* 41 }
       it { expect(cafe).not_to be_valid }
     end
   end
