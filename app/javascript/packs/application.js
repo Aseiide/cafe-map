c43224e1c67e78f6a994f6472869eda6a7ede043
Aseiide <eiseiide@gmail.com>
2021-04-19 18:16:09 +0900

    fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,6 @@ import 'bootstrap';
 import '../stylesheets/application.scss';
 import '../stylesheets/admin/application';
 
-
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
