ca08d8341f80ea3f228c8c80f570c6d733aa1cf1
Aseiide <eiseiide@gmail.com>
2021-05-07 00:24:29 +0900

    developmentのためバリデーションを外す

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -1,5 +1,4 @@
 class Cafe < ApplicationRecord
-  validates:address, presence: true, length: {maximum: 50}, format: {
-    with: /...??[都道府県]/}
+  validates:address, presence: true, length: {maximum: 50}
   validates:name, presence: true, length: {maximum: 40}
 end
