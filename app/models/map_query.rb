a09560781a8158a3fb80443b1760c2b0924befd2
Aseiide <eiseiide@gmail.com>
2021-05-06 23:43:54 +0900

    statusを取得できるように実装

--- app/models/map_query.rb
+++ app/models/map_query.rb
@@ -6,7 +6,13 @@ class MapQuery
 
   def uri
     address = URI.encode_www_form({address: @cafe_param})
-    URI.parse("https://maps.googleapis.com/maps/api/geocode/json?#{address}&key=#{ENV["MAP_API_KEY"]}")
+    URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{ENV["MAP_API_KEY"]}")
+  end
+
+  def status
+    api_response = Net::HTTP.get_response(uri)
+    response_body = JSON.parse(api_response.body)
+    response_body["status"]
   end
 
   def result
